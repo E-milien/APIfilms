6ca4be0149880eae7d4b84e9753dcf39156d7a19
roukinee <roukinee@D101-S16>
2024-02-16 16:17:17 +0100

    validation

---
+++ APIfilms/Models/Repository/IDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIfilms.Models.Repository
+{
+    public interface IDataRepository
+    {
+        public interface IDataRepository<TEntity>
+        {
+            ActionResult<IEnumerable<TEntity>> GetAll();
+            ActionResult<TEntity> GetById(int id);
+            ActionResult<TEntity> GetByString(string str);
+            void Add(TEntity entity);
+            void Update(TEntity entityToUpdate, TEntity entity);
+            void Delete(TEntity entity);
+        }
+    }
+}
